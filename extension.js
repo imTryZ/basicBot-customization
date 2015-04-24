3e752533762da462d8477d556c083b0f2a8e4ca1
Motel バイブル <fesnandu@live.it>
2015-04-23 21:05:25 -0300

    Update extension.js

--- extension.js
+++ extension.js
@@ -35,7 +35,7 @@
                 
                 bot.commands.cookieCommand = {
             command: 'cookie',
-            rank: 'mod',
+            rank: 'user',
             type: 'exact',
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
